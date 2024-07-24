fdd303b82ea9c3b83c8789c50c3ed766d0fc496b
julie <juliemytruong@gmail.com>
2024-07-24 13:50:20 +1000

    e

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -7,13 +7,33 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window= Tk()
 window.title("password saver")
-window.config(padx=50, pady=50)
+window.geometry("500x500")
+window.config(padx=20, pady=20)
 
-canvas=Canvas(width=500, height=500)
+canvas=Canvas(width=250, height=200)
 logo=PhotoImage(file="logo.png")
-canvas.create_image(250,200,image=logo)
-canvas.grid(column=1,row=1)
+canvas.create_image(150,100,image=logo)
+canvas.grid(column=2,row=1)
 
+label_web=Label(text="Website:")
+label_web.grid(column=1,row=2)
 
+entry_web=Entry(width=35)
+entry_web.grid(column=2,row=2,columnspan=2)
+
+label_user=Label(text="Username:")
+label_user.grid(column=1,row=3)
+
+entry_user=Entry(width=35)
+entry_user.grid(column=2,row=3,columnspan=2)
+
+label_pw=Label(text="password:")
+label_pw.grid(column=1,row=4)
+
+entry_pw=Entry(width=21)
+entry_pw.grid(column=2,row=4)
+
+button = Button(text="generate")
+button.grid(column=3,row=4)
 
 window.mainloop()
