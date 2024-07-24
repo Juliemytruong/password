1e3eb26facd7e25b83ae1438bf17d062266c76d8
julie <juliemytruong@gmail.com>
2024-07-24 23:25:40 +1000

    e

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -7,33 +7,36 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window= Tk()
 window.title("password saver")
-window.geometry("500x500")
-window.config(padx=20, pady=20)
+#window.geometry("500x500")
+window.config(padx=50, pady=50)
 
-canvas=Canvas(width=250, height=200)
+canvas=Canvas(width=200, height=200)
 logo=PhotoImage(file="logo.png")
-canvas.create_image(150,100,image=logo)
-canvas.grid(column=2,row=1)
+canvas.create_image(100,100,image=logo)
+canvas.grid(column=2,row=0)
 
 label_web=Label(text="Website:")
-label_web.grid(column=1,row=2)
+label_web.grid(column=1,row=1)
 
-entry_web=Entry(width=35)
-entry_web.grid(column=2,row=2,columnspan=2)
+entry_web=Entry(width=38)
+entry_web.grid(column=2,row=1,columnspan=2)
 
 label_user=Label(text="Username:")
 label_user.grid(column=1,row=3)
 
-entry_user=Entry(width=35)
+entry_user=Entry(width=38)
 entry_user.grid(column=2,row=3,columnspan=2)
 
-label_pw=Label(text="password:")
+label_pw=Label(text="Password:")
 label_pw.grid(column=1,row=4)
 
-entry_pw=Entry(width=21)
+entry_pw=Entry(width=25)
 entry_pw.grid(column=2,row=4)
 
-button = Button(text="generate")
-button.grid(column=3,row=4)
+button_gen = Button(text="generate",width=8)
+button_gen.grid(column=3,row=4)
+
+button_add = Button(text="add",width=33)
+button_add.grid(column=2,row=5,columnspan=2)
 
 window.mainloop()
