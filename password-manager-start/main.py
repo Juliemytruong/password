e93816e7a8993889923998c43325eff28b5bbad8
julie <juliemytruong@gmail.com>
2024-07-25 20:41:27 +1000

    e

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -4,7 +4,11 @@ import random
 import string
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def gen_pw():
-
+    global entry_pw
+    res = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+    entry_pw.delete(0,END)
+    entry_pw.insert(0,f"{res}")
+    print(res)
 
 
 
@@ -40,6 +44,7 @@ label_user.grid(column=1,row=3)
 
 entry_user=Entry(width=38)
 entry_user.grid(column=2,row=3,columnspan=2)
+
 entry_user.insert(0,"juliemytruong@gmail.com")
 
 label_pw=Label(text="Password:")
@@ -48,7 +53,7 @@ label_pw.grid(column=1,row=4)
 entry_pw=Entry(width=25)
 entry_pw.grid(column=2,row=4)
 
-button_gen = Button(text="generate",width=8)
+button_gen = Button(text="generate",width=8,command=gen_pw)
 button_gen.grid(column=3,row=4)
 
 button_add = Button(text="add",width=33,command=add_to_file)
