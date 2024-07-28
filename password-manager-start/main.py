6cde277c2b7f8b1a0ab9e985e0206853f4980f78
julie <juliemytruong@gmail.com>
2024-07-28 23:44:36 +1000

    e

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -49,7 +49,7 @@ def add_to_file():
 
     text_for_add={
         entry_web.get():{
-            "email":entry_user.get(),
+            "user name":entry_user.get(),
             "password":entry_pw.get()
         }
     }
@@ -58,12 +58,48 @@ def add_to_file():
     if len(entry_web.get())==0 or len(entry_pw.get())==0:
         messagebox.showinfo("check data","check for empty cells")
     else:
-            f = open("password.json", "w")
-            json.dump(text_for_add,f,indent=4)
-            f.close()
 
-    entry_web.delete(0,END)
-    entry_pw.delete(0,END)
+        with open("password.json", "r") as f:
+            data=json.load(f)
+        print(data)
+
+        with open("password.json","w"):
+
+            data.update(text_for_add)
+            json.dump(data,f,indent=4)
+
+        f.close()
+
+
+
+        entry_web.delete(0,END)
+        entry_pw.delete(0,END)
+
+
+
+#----------------------------findPW---------------------------------------#
+
+def find_password():
+    with open("password.json","r") as dataA:
+        datap=json.load(dataA)
+    print(datap)
+
+    try:
+        return_data=datap[entry_web.get()]
+        print(return_data)
+        return_user=return_data["email"]
+        return_pw=return_data["password"]
+    except KeyError:
+        messagebox.showinfo("return info","no password created")
+    else:
+        messagebox.showinfo("return info",f"your user name is {return_user} and your passwords is {return_pw}")
+
+
+
+
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window= Tk()
@@ -79,9 +115,9 @@ canvas.grid(column=2,row=0)
 label_web=Label(text="Website:")
 label_web.grid(column=1,row=1)
 
-entry_web=Entry(width=38)
+entry_web=Entry(width=25)
 entry_web.focus()
-entry_web.grid(column=2,row=1,columnspan=2)
+entry_web.grid(column=2,row=1,columnspan=1)
 
 label_user=Label(text="Username:")
 label_user.grid(column=1,row=3)
@@ -103,7 +139,8 @@ button_gen.grid(column=3,row=4)
 button_add = Button(text="add",width=33,command=add_to_file)
 button_add.grid(column=2,row=5,columnspan=2)
 
-
+button_search = Button(text="search",width=8,command=find_password)
+button_search.grid(column=3,row=1)
 
 
 window.mainloop()
